4e55fa18d235866f67d297c8abcf4b96087223fe
Elliot Woods <elliot.woods@gmail.com>
2017-09-04 21:28:23 +0200

    first commit - working

---
+++ src/ofxArUco.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "ofMain.h"
+
+//this addon doesn't do anything but provide libs to use directly
+
+class ofxArUco {
+public:
+	static string whatDoesThisAddonDo();
+};
---
+++ src/ofxAruco.cpp
@@ -0,0 +1,6 @@
+#include "ofxArUco.h"
+
+//----------
+string ofxArUco::whatDoesThisAddonDo() {
+	return "this addon doesn't do anything but provide libs to use directly";
+}
